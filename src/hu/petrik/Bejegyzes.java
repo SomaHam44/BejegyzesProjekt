b89e61fa35dac0b38e31d2eb2fbcaa701da49811
Your Name <you@example.com>
2021-09-29 14:52:44 +0200

    Bejegyzes osztaly létrehozása és a privát adattagok megadása

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -1,4 +1,11 @@
 package hu.petrik;
 
+import java.time.LocalDateTime;
+
 public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
 }

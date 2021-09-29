4fd5f4be748420ad4a46e4e67fdafc2e55d59c6e
Your Name <you@example.com>
2021-09-29 15:22:55 +0200

    Két bejegyzést tartalmazó lista felvétele

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,8 +1,16 @@
 package hu.petrik;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
+    static List<Bejegyzes> bejegyzesLista = new ArrayList<>();
 
     public static void main(String[] args) {
-	// write your code here
+        Bejegyzes bejegyzes1 = new Bejegyzes("Lee","Igen, ez jó");
+        Bejegyzes bejegyzes2 = new Bejegyzes("Pee","Nem jó");
+	    bejegyzesLista.add(bejegyzes1);
+	    bejegyzesLista.add(bejegyzes2);
     }
 }

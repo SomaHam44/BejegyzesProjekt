6e86eea3b04271e3860a6eb4af65a64a0168d7dd
Your Name <you@example.com>
2021-09-29 15:31:00 +0200

    Darabszám bekérése, ugyanennyi új bejegyzés létrehozása és listához adása

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -2,15 +2,28 @@ package hu.petrik;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Main {
     static List<Bejegyzes> bejegyzesLista = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         Bejegyzes bejegyzes1 = new Bejegyzes("Lee","Igen, ez jó");
         Bejegyzes bejegyzes2 = new Bejegyzes("Pee","Nem jó");
 	    bejegyzesLista.add(bejegyzes1);
 	    bejegyzesLista.add(bejegyzes2);
+
+        System.out.print("Kérek egy darabszámot: ");
+        int darabszam = sc.nextInt();
+        for (int i = 0; i < darabszam; i++) {
+            Bejegyzes b = new Bejegyzes("Sz","Létrehozás");
+            bejegyzesLista.add(b);
+        }
+
+
     }
+
+
 }

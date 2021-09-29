996409824b084b99273a244f633461d42c647bdb
Your Name <you@example.com>
2021-09-29 14:55:43 +0200

    Konstruktor elkészítése

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -8,4 +8,13 @@ public class Bejegyzes {
     private int likeok;
     private LocalDateTime letrejott;
     private LocalDateTime szerkesztve;
+
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok = 0;
+        this.letrejott = LocalDateTime.now();
+        this.szerkesztve = LocalDateTime.now();
+    }
 }
